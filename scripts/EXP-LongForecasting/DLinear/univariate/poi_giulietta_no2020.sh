33e479515635c1fa09e4aaf833bb81320b3028ff
Mariodraghidicomodo <alessandropedron98@gmail.com>
2025-06-06 12:16:55 +0200

    test before 2020

---
+++ scripts/EXP-LongForecasting/DLinear/univariate/poi_giulietta_no2020.sh
@@ -0,0 +1,32 @@
+if [ ! -d "./logs" ]; then
+    mkdir ./logs
+fi
+
+if [ ! -d "./logs/LongForecasting" ]; then
+    mkdir ./logs/LongForecasting
+fi
+
+if [ ! -d "./logs/LongForecasting/univariate" ]; then
+    mkdir ./logs/LongForecasting/univariate
+fi
+seq_len=96
+label_len=24
+pred_len=1
+
+python -u run_longExp.py \
+  --is_training 1 \
+  --root_path ./dataset/final_15min_df_poi/ \
+  --data_path frequency_15min_df_61_no2020.csv \
+  --model_id freq_df_61_no2020_15min_$seq_len'_'$pred_len'_'$label_len'_'S \
+  --model DLinear \
+  --data custom \
+  --features S \
+  --target frequency \
+  --freq 15min \
+  --patience 10\
+  --train_epochs 100 \
+  --seq_len $seq_len \
+  --label_len $label_len \
+  --pred_len $pred_len \
+  --des 'Exp' \
+  --itr 1 --batch_size 30 --feature S --learning_rate 0.005 >logs/LongForecasting/DLinear_freq_df_61_no2020_15min_Giulietta_2018_$seq_len'_'$pred_len'_'$label_len'_'S
